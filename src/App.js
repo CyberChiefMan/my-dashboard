d515c5ec57e73e655eced9a265cc3b3241b7ac54
mehdi <mahmoudi.m2@gmail.com>
2019-02-16 22:40:19 +0330

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+class App extends Component {
+  render() {
+    return (
+      <Button variant="contained" color="secondary">
+        my-dashboard
+      </Button>
+    );
+  }
+}
+
+export default App;
